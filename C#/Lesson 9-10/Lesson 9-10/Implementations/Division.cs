a174ecb25140a5cd9295bf1ae97230b9f0cb64c2
Faik Hasanov <faik.hasanov05@gmail.com>
2024-12-23 12:54:59 +0400

    Lesson 9-10

---
+++ C#/Lesson 9-10/Lesson 9-10/Implementations/Division.cs
@@ -0,0 +1,27 @@
+﻿using Lesson_9_10.Interfaces;
+
+namespace Lesson_9_10.Implementations;
+
+public class Division : ICalculatorOperation
+{
+    public double Execute(double a, double b)
+    {
+        try
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return 0;
+        }
+       
+    }
+    
+    public string Name => "Division";
+    public string OperationSymbol => "/";
+}
---
+++ C#/Lesson 9-10/Lesson 9-10/Implementations/Subtraction.cs
@@ -0,0 +1,14 @@
+﻿using Lesson_9_10.Interfaces;
+
+namespace Lesson_9_10.Implementations;
+
+public class Subtraction : ICalculatorOperation
+{
+    public double Execute(double a, double b)
+    {
+        return a - b;
+    }
+    
+    public string Name => "Subtraction";
+    public string OperationSymbol => "-";
+}
