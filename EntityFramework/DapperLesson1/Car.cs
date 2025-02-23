421e6b54e2ed73f1a22aa8a7dd199cf192d106a4
Faik Hasanov <faik.hasanov05@gmail.com>
2025-02-23 22:14:23 +0400

    Dapper(L1)

---
+++ EntityFramework/DapperLesson1/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperLesson1;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id} | {Brand} | {Model} | {Year} | {Price}";
+    }
+}
