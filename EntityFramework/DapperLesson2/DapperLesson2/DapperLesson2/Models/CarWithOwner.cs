4b8e217dbd07f64ac1cf5ceab68504e676ff5c1c
Faik Hasanov <faik.hasanov05@gmail.com>
2025-02-28 15:59:20 +0400

    DapperLesson2

---
+++ EntityFramework/DapperLesson2/DapperLesson2/DapperLesson2/Models/CarWithOwner.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperLesson2;
+
+public class CarWithOwner
+{
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+    public string Name { get; set; }
+}
