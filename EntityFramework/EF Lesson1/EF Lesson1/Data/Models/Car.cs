13e8ef4d53ff8f9cb41bb2e4f489761f9126b577
Faik Hasanov <faik.hasanov05@gmail.com>
2025-03-01 18:22:41 +0400

    EF Lesson1

---
+++ EntityFramework/EF Lesson1/EF Lesson1/Data/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_Lesson1.Data.Models;
+
+public class Car
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Brand { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Model { get; set; }
+    
+    public DateTime ProductionYear { get; set; } = DateTime.Now;
+    public decimal Price { get; set; }
+}
