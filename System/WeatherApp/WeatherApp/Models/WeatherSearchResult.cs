1bf4b14b9a4cf03ea0d9205f1da39b5370f1859e
Faik Hasanov <faik.hasanov05@gmail.com>
2025-04-09 23:28:56 +0400

    WeatherAPI

---
+++ System/WeatherApp/WeatherApp/Models/WeatherSearchResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WeatherApp.Models.WeatherInfo;
+
+namespace WeatherApp.Models;
+
+internal class WeatherSearchResult
+{
+    public coord coord { get; set; }
+    public weather[] weather { get; set; }
+    public main main { get; set; }
+    public wind wind { get; set; }
+    public sys sys { get; set; }
+
+}
