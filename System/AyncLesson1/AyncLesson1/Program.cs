d6a0395319a6ef767dc1440db8ef413a2054a325
Faik Hasanov <faik.hasanov05@gmail.com>
2025-04-12 17:11:12 +0400

    AsyncLesson1

---
+++ System/AyncLesson1/AyncLesson1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using AyncLesson1.Methods;
+
+Random random = new Random();
+
+List<int> nums = new List<int>();
+
+for (int i = 0; i < 20; i++)
+{
+    
+    int number = random.Next(1, 101);
+    
+    nums.Add(number);
+}
+
+Console.WriteLine("Full list:");
+AllMethods.PrintNums(nums);
+
+var evenNums = await AllMethods.FilterEvenAsync(nums);
+var oddNums = await AllMethods.FilterOddAsync(nums);
+var sumNums = await AllMethods.CalculateSumAsync(nums);
+
+Console.Write("\nEven numbers: ");
+AllMethods.PrintNums(evenNums);
+
+Console.Write("\nOdd numbers: ");
+AllMethods.PrintNums(oddNums);
+
+Console.Write($"\nSum: {sumNums}");
+
