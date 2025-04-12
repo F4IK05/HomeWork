ee2333236a27facaae3aeeb99d8c1421edf0694a
Faik Hasanov <faik.hasanov05@gmail.com>
2025-04-12 17:11:12 +0400

    AsyncLesson1

---
+++ System/AyncLesson1/AyncLesson1/Methods/AllMethods.cs
@@ -0,0 +1,39 @@
+﻿namespace AyncLesson1.Methods;
+
+public class AllMethods
+{
+    public static void PrintNums(List<int> nums)
+    {
+        foreach (var num in nums)
+        {
+            Console.Write($"{num} ");
+        }
+    }
+
+    public static async Task<List<int>> FilterEvenAsync(List<int> numbers)
+    {
+        return await Task.Run(async () =>
+        {
+            await Task.Delay(1500);
+            return numbers.Where(num => num % 2 == 0).ToList();
+        });
+    }
+
+    public static async Task<List<int>> FilterOddAsync(List<int> numbers)
+    {
+        return await Task.Run(async () =>
+        {
+            await Task.Delay(1500);
+            return numbers.Where(num => num % 2 != 0).ToList();
+        });
+    }
+
+    public static async Task<int> CalculateSumAsync(List<int> numbers)
+    {
+        return await Task.Run(async () =>
+        {
+            await Task.Delay(1000);
+            return numbers.Sum();
+        });
+    }
+}
