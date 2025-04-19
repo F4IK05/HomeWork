953b3f0a79f7f33adeb122510e955ae65576a97d
Faik Hasanov <faik.hasanov05@gmail.com>
2025-04-20 01:00:59 +0400

    AsyncLesson2

---
+++ System/AsyncLesson2/AsyncLesson2/AllMethods.cs
@@ -0,0 +1,66 @@
+﻿using AsyncLesson2.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncLesson2;
+
+public static class AllMethods
+{
+    public static void AddStudentManually(string name)
+    {
+        Task.Run(() =>
+        {
+            using var context = new StudentContext();
+            Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            var user = new Student
+            {
+                Name = name,
+            };
+            
+            context.Students.Add(user);
+            context.SaveChanges();
+        }).GetAwaiter().GetResult();
+    }
+
+    public static void ShowAllStudentsManually()
+    {
+        var students = Task.Run(() =>
+        {
+            using var context = new StudentContext();
+            Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            return context.Students.ToList();
+            
+        }).GetAwaiter().GetResult();
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Id}: {student.Name}");
+        }
+    }
+
+    public static async Task AddStudentAsync(string name)
+    {
+        using var context = new StudentContext();
+        Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+        
+        var user = new Student
+        {
+            Name = name,
+        };
+
+        context.Students.Add(user);
+        await context.SaveChangesAsync();
+    }
+
+    public static async Task ShowAllStudentsAsync()
+    {
+        using var context = new StudentContext();
+        Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+
+        var students = await context.Students.ToListAsync();
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Id}: {student.Name}");
+        }
+    }
+}
---
+++ System/Test/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿void DoWork()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Поток: {Thread.CurrentThread.ManagedThreadId} — {i}");
+        Thread.Sleep(500); // пауза
+    }
+}
+
+Thread t1 = new Thread(DoWork);
+Thread t2 = new Thread(DoWork);
+
+t1.Start(); // запускаем поток 1
+t2.Start(); // запускаем поток 2
+
+DoWork(); // выполняем в главном потоке
