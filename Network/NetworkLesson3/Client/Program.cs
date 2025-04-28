61d717f0c3da712f49930c4e7f9b253525ef2112
Faik Hasanov <faik.hasanov05@gmail.com>
2025-04-28 18:34:49 +0400

    NetworkLesson3

---
+++ Network/NetworkLesson3/Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Net.Sockets;
+
+bool isRunning = true;
+
+Console.Write("Введите IpAddress другого пользователя(сервера): ");
+string? ipAddress = Console.ReadLine();
+
+Console.Write("Введите Port другого пользователя(сервера): ");
+if (!int.TryParse(Console.ReadLine(), out int port))
+{
+    Console.WriteLine("Неправильный формат порта.");
+    return;
+}
+
+try
+{
+    using TcpClient client = new TcpClient();
+    if (ipAddress != null) client.Connect(ipAddress, port);
+
+    Console.WriteLine("\nПодключение к серверу установлено!");
+    Thread.Sleep(500);
+    Console.Clear();
+    
+    using NetworkStream stream = client.GetStream();
+    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+    StreamReader reader = new StreamReader(stream);
+
+    Task.Run(() =>
+    {
+        while (isRunning)
+        {
+            string? sMessage = reader.ReadLine();
+            if (sMessage == null)
+            {
+                Console.WriteLine("\nСоединение закрыто.");
+                isRunning = false;
+                break;
+            }
+            
+            var (left, top) = Console.GetCursorPosition();
+            
+            Console.SetCursorPosition(0, top);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, top);
+            
+            Console.WriteLine($"Сервер: {sMessage}");
+            
+            Console.Write("Вы: ");
+        }
+    });
+
+    Console.WriteLine("=====Чат=====");
+    while (isRunning)
+    {
+        Console.Write("Вы: ");
+        string? message = Console.ReadLine();
+        writer.WriteLine(message);
+
+        if (message?.ToLower() == "quit")
+        {
+            isRunning = false;
+            break;
+        }
+    }
+    
+    client.Close();
+    stream.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Проверьте правильность IP адреса и порта.");
+}
---
+++ Network/NetworkLesson3/Server/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace NetworkLesson3;
+
+public class Methods
+{
+    public string GetLocalIpAddress()
+    {
+        string hostName = Dns.GetHostName();
+        string ipAddress = Dns.GetHostEntry(hostName)
+            .AddressList
+            .First(a => a.AddressFamily == AddressFamily.InterNetwork)
+            .ToString();
+        
+        return ipAddress;
+    }
+}
