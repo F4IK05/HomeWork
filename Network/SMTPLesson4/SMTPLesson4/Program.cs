9194bf634d5652e862e9665e7d7a45feb06ae3cc
Гасанов Фаиг Эльшад <Gasan_fi51@itstep.edu.az>
2025-05-04 17:24:55 +0400

    NetworkLesson4

---
+++ Network/SMTPLesson4/SMTPLesson4/Program.cs
@@ -0,0 +1,21 @@
+﻿using SMTPLesson4.Classes;
+
+var sender = new EmailSender();
+
+Console.Write("Ваш e-mail: ");
+var emailSender = Console.ReadLine();
+
+Console.Write("E-mail получателя: ");
+var emailRecipient = Console.ReadLine();
+
+Console.WriteLine("Заговолок: ");
+var emailSubject = Console.ReadLine();
+
+Console.WriteLine("Тело: ");
+var emailBody = Console.ReadLine();
+
+var message = new EmailMessage(emailSender, emailRecipient, emailSubject, emailBody);
+if (sender.Send(message))
+{
+    Console.WriteLine("Сообщение передано успешно!");
+};
