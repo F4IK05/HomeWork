2a1587505677c4db623c90d49c812471cb7a5f19
Faik Hasanov <faik.hasanov05@gmail.com>
2025-06-10 17:00:24 +0400

    JS Project

---
+++ JS/Project/public/mainscript.js
@@ -0,0 +1,38 @@
+const dropdown = document.querySelector('.categories');
+const toggle = document.querySelector('.active-cat');
+const dropdownArrow = document.querySelector('.dropdown-arrow');
+const searchField = document.querySelector('#searchField');
+const searchBtn = document.querySelector('#searchBtn');
+const searchInput = document.querySelector('#searchInput');
+
+// Переключение выпадающего списка(аддаптивность)
+toggle.addEventListener('click', () => {
+    dropdown.classList.toggle('open');
+    dropdownArrow.style.transform = 'rotate(180deg)';
+    if (!dropdown.classList.contains('open')) {
+        dropdownArrow.style.transform = 'rotate(0deg)';
+    }
+});
+
+// Закрытие выпадающего списка и поля поиска при клике вне их
+window.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!dropdown.contains(target)) {
+        dropdown.classList.remove('open');
+        dropdownArrow.style.transform = 'rotate(0deg)';
+    }
+    if (!searchField.contains(target) && !searchBtn.contains(target)) {
+        searchField.classList.remove('active');
+    }
+});
+
+// Открытие/закрытие поиска по клику
+searchBtn.addEventListener('click', () => {
+    if (searchField.classList.contains('active')) {
+        searchField.classList.remove('active');
+    }
+    else {
+        searchField.classList.add('active');
+        searchInput.focus();
+    }
+});
