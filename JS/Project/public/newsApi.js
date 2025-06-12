61c503a93765a8f4a90ff996329b3656e02af1eb
Faik Hasanov <faik.hasanov05@gmail.com>
2025-06-12 17:51:11 +0400

    JS Project

--- JS/Project/public/newsApi.js
+++ JS/Project/public/newsApi.js
@@ -1,6 +1,32 @@
-async function fetchNews() {
+function fetchMainNews() {
     const API_key = 'apiKey=adb1151f8f244e8db0d8577b7d0302fd';
-    const url = `https://newsapi.org/v2/top-headlines?country=us&${API_key}`;
+    const fromDate = '2025-05-11';
+    const query = 'news';
+    const url = `https://newsapi.org/v2/everything?q=${query}&from=${fromDate}&sortBy=popularity&${API_key}`;
+
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        const articles = data.articles;
+
+       renderMainNews(articles)
+
+    })
+    .catch(error => {
+        console.error(error)
+    });
+}
+
+
+function fetchLatestNews() {
+    const API_key = 'apiKey=adb1151f8f244e8db0d8577b7d0302fd';
+    const url = `https://newsapi.org/v2/top-headlines?country=us&sortby=publeshedAt&${API_key}`;
 
     
     fetch(url)
@@ -12,15 +38,18 @@ async function fetchNews() {
         return response.json();
     })
     .then(data => {
-        renderNews(data.articles)
+        const articles = data.articles;
+
+        renderOtherNews(articles.slice(0,5))
+
     })
     .catch(error => {
         console.error(error)
     });
 }
 
-function renderNews(articles) {
-    const container = document.querySelector('.news-container');
+function renderMainNews(articles) {
+    const container = document.querySelector('.main-news-container');
 
     articles.forEach(article => {
         const card = document.createElement('div');
@@ -30,11 +59,32 @@ function renderNews(articles) {
             <a href="${article.url}" target="_blank"><h2>${article.title}</h2></a>
             <a href="${article.url}" target="_blank">${article.urlToImage ? `<img src="${article.urlToImage}" alt="news image"/>` : ''}</a>
             <p>${article.description || ''}</p>
-            <span>${article.publishedAt}</span>
+            <span>${new Date(article.publishedAt).toLocaleString()}</span>
         `;
 
-        container.appendChild(card)
+        container.appendChild(card);
+    })
+};
+
+
+function renderOtherNews(articles) {
+    const list = document.querySelector('.latest-news-list');
+
+    articles.forEach(article => {
+        const li = document.createElement('li');
+        li.className = 'last-news-card';
+
+        li.innerHTML = `
+            <a href="${article.url}" target="_blank">
+                <h3>${article.title}</h3>
+                <p>${article.description}</p>
+            </a>
+        `;
+
+        list.appendChild(li)
     });
 }
 
-document.addEventListener('DOMContentLoaded', fetchNews);
+document.addEventListener('DOMContentLoaded', () => {
+    fetchLatestNews(), 
+    fetchMainNews()});
