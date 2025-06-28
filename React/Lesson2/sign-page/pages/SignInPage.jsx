748313e32a89b3f03b2251e4a3809cc05c103c79
Faik Hasanov <faik.hasanov05@gmail.com>
2025-06-28 23:31:18 +0400

    Lesson 2

--- React/Lesson2/sign-page/pages/SignInPage.jsx
+++ React/Lesson2/sign-page/pages/SignInPage.jsx
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 class SignInPage extends Component {
     render() {
         return (
-            <div className='block'>
-                
+            <div className="container">
+                <form action="">
+                    <h1>Sign in</h1>
+                    <label>Username</label>
+                    <input type="text" />
+
+                    <label>Password</label>
+                    <input type="password" />
+
+                    <button type='submit'>Submit</button>
+                </form>
             </div>
         );
     }
