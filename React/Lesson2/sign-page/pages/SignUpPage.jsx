170d279113a79a60934d0e6b2655701e5e2dd7c4
Faik Hasanov <faik.hasanov05@gmail.com>
2025-06-28 23:31:18 +0400

    Lesson 2

--- React/Lesson2/sign-page/pages/SignUpPage.jsx
+++ React/Lesson2/sign-page/pages/SignUpPage.jsx
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class SignUpPage extends Component {
     render() {
         return (
-            <div>
-                <h2>Sign up</h2>
+            <div className="container">
+                <form action="">
+                    <h1>Sign up</h1>
+                    <label>Username</label>
+                    <input type="text" />
+
+                    <label>Email</label>
+                    <input type="email" />
+
+                    <label>Password</label>
+                    <input type="password" />
+
+                    <button type='submit'>Submit</button>
+                </form>
             </div>
         );
     }
