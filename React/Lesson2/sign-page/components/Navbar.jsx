4bd84b183373793cd9fe281dfc169ce88d95c4c7
Faik Hasanov <faik.hasanov05@gmail.com>
2025-06-29 21:33:25 +0400

    Lesson2

--- React/Lesson2/sign-page/components/Navbar.jsx
+++ React/Lesson2/sign-page/components/Navbar.jsx
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import SignInPage from '../pages/SignInPage';
-import SignUpPage from '../pages/SignUpPage';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css'
 
 class Navbar extends Component {
     render() {
         return (
-            <Router>
-                <div className='navbar'>
-                    <ul>
-                        <li><Link to="/">Sign in</Link></li>
-                        <li><Link to="/sign-up">Sign up</Link></li>
-                    </ul>
-                </div>
-
-                <Routes>
-                    <Route path='/' exact element={<SignInPage/>}/>
-                    <Route path='/sign-up' element={<SignUpPage/>}/>
-                </Routes>
-            </Router>
+            <div className='navbar'>
+                <ul>
+                    <li><Link to="/">Sign in</Link></li>
+                    <li><Link to="/sign-up">Sign up</Link></li>
+                </ul>
+            </div>
         );
 
 
---
+++ React/Lesson2/sign-page/components/SignUpForm.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class SignUpPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log("Username: ", this.state.username)
+        console.log("Email: ", this.state.email)
+        console.log("Password: ", this.state.password)
+    }
+
+
+    render() {
+        return (
+            <form action="" onSubmit={this.handleSubmit}>
+                <h1>Sign up</h1>
+                <label>Username</label>
+                <input type="text" name='username' onChange={this.handleChange} value={this.state.username} />
+
+                <label>Email</label>
+                <input type="email" name='email' onChange={this.handleChange} value={this.state.email} />
+
+                <label>Password</label>
+                <input type="password" name='password' onChange={this.handleChange} value={this.state.password} />
+
+                <button type='submit'>Submit</button>
+            </form>
+        );
+    }
+}
+
+export default SignUpPage;
