89dc0fe8d23621d54449f90f48c7cdf6c8347dd3
Faik Hasanov <faik.hasanov05@gmail.com>
2025-06-29 21:33:25 +0400

    Lesson2

--- React/Lesson2/sign-page/pages/SignInPage.jsx
+++ React/Lesson2/sign-page/pages/SignInPage.jsx
@@ -1,40 +1,12 @@
 import React, { Component } from 'react';
+import SignInForm from '../components/SignInForm'
+import '../styles/SignPages.css'
 
 class SignInPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-
-        console.log("Username: ", this.state.username)
-        console.log("Password: ", this.state.password)
-    }
-
-
     render() {
         return (
-            <div className="container">
-                <form action="" onSubmit={this.handleSubmit}>
-                    <h1>Sign in</h1>
-                    <label>Username</label>
-                    <input type="text" name='username' onChange={this.handleChange} value={this.state.username}/>
-
-                    <label>Password</label>
-                    <input type="password" name='password' onChange={this.handleChange} value={this.state.password}/>
-
-                    <button type='submit'>Submit</button>
-                </form>
+            <div className='container'>
+                <SignInForm/>
             </div>
         );
     }
--- React/Lesson2/sign-page/src/App.jsx
+++ React/Lesson2/sign-page/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Navbar from '../components/Navbar'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SignInPage from '../pages/SignInPage'
+import SignUpPage from '../pages/SignUpPage'
 
 function App() {
 
   return (
-    <Navbar/>
+    <Router>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<SignInPage/>}/>
+        <Route path='/sign-up' element={<SignUpPage/>}/>
+      </Routes>
+
+    </Router>
   )
 }
 
