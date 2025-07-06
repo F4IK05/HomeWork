d33a11fb57af3c56210fec47f5b7d4aeccc7f641
Faik Hasanov <faik.hasanov05@gmail.com>
2025-07-06 20:55:48 +0400

    Lesson 3

--- React/Lesson3/movie-app/src/components/Navbar.jsx
+++ React/Lesson3/movie-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import SignButton from './SignButton';
 
 const Navbar = ({setValue}) => {
     return (
-        <div className="w-full bg-white text-black">
+        <div className="fixed top-0 w-full bg-white text-black z-[1000]">
             <div className="px-10">
                 <div className="flex justify-between h-16 items-center">
                     <div className="text-xl font-bold">
