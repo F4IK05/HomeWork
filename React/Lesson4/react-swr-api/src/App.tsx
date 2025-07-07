b82a06fc2c1fc34ca1d0096b498b0a3b6f1e55a6
Faik Hasanov <faik.hasanov05@gmail.com>
2025-07-07 22:50:31 +0400

    Lesson 4

---
+++ React/Lesson4/react-swr-api/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CatBreedSearch from './components/CatBreedSearch'
+
+
+function App() {
+  return (
+    <>
+      <CatBreedSearch/>
+    </>
+  )
+}
+
+export default App
