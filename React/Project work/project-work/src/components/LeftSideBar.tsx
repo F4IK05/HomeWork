25b5601738d8e020a298accd153d47ff32f68257
Faik Hasanov <faik.hasanov05@gmail.com>
2025-07-13 01:41:50 +0400

    Project

--- React/Project work/project-work/src/components/LeftSideBar.tsx
+++ React/Project work/project-work/src/components/LeftSideBar.tsx
@@ -1,44 +1,99 @@
-import { ChevronFirst, ChevronLast, Settings } from "lucide-react";
-import React, { createContext, useState } from "react";
-import { UserIcon } from '@heroicons/react/24/solid';
+import { ChevronFirst } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
+import { SideBarContext } from "@/contexts/SideBarContext";
+// import { UserIcon } from '@heroicons/react/24/solid';
 
-export const SideBarContext = createContext();
+const LeftSideBar: React.FC<{ children: React.ReactNode; isOpen: boolean; setIsOpen }> = ({ children, isOpen, setIsOpen }) => {
+    const sideBarRef = useRef<HTMLDivElement>(null);
+    const [isMobile, setIsMobile] = useState(false);
 
-const LeftSideBar: React.FC = ({ children }) => {
-    const [isOpen, setIsOpen] = useState(true);
+    useEffect(() => {
+        const hadleResize = () => {
+            setIsOpen(window.innerWidth >= 768);
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        hadleResize()
+
+        window.addEventListener("resize", hadleResize);
+
+        return () => window.removeEventListener("resize", hadleResize);
+    }, [setIsOpen]); // если заходят с телефонных размеров закрывать 
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (isOpen && sideBarRef.current && !sideBarRef.current.contains(e.target as Node) && isMobile) {
+                setIsOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isMobile, isOpen, setIsOpen])
+
+    useEffect(() => {
+        if (isOpen && isMobile) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = ''
+        }
+    }, [isOpen, isMobile])
 
     return (
-        <div className="h-screen text-white sm:text-lg md:text-base lg:text-sm">
-            <div className="h-full flex flex-col bg-[#212124]">
-                <div className={`border-b border-[#63676e] p-4 flex ${isOpen ? 'justify-end' : 'justify-center'} items-center`}>
-                    <button onClick={() => setIsOpen((val) => !val)} className="cursor-pointer">
-                        {isOpen ? <ChevronFirst className="rounded-lg w-10 h-10 bg-[#63676e] p-1.5"/>
-                        : <ChevronLast className="rounded-lg w-10 h-10 bg-[#63676e] p-1.5"/>}
-                    </button>
-                </div>
-                
-                <SideBarContext.Provider value={{isOpen}}>
-                    <ul className="flex-1 m-2">
-                        {children}
-                    </ul>
-                </SideBarContext.Provider>
+        <>
+            {/* Затемнение фона */}
+            {isOpen && isMobile && (
+                <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+            )}
 
-
-                <div className="border-t border-[#63676e] flex justify-center items-center p-2">
-                    
-                    <UserIcon className="w-12 h-12 p-2 bg-[#63676e] text-white rounded-[50%]"/>
-                    
-                    <div className={`cursor-pointer hover:bg-gray-400 hover:rounded p-1 overflow-hidden transition-all ${isOpen ? 'w-32 ml-3' : 'w-0 hidden'}`}>
-                        <div>
-                            <p>Name</p>
-                            <p>example.com</p>
-                        </div>
-            
+            <div
+                ref={sideBarRef}
+                className={`
+                    h-screen transition-all fixed z-50 text-gray-400
+                    ${isOpen ? 'w-64' : 'w-16'}
+                    ${!isOpen && isMobile ? 'opacity-0 -translate-x-full' : 'translate-x-0'}
+                `}>
+                <div className="h-full flex flex-col bg-[#212124]">
+                    <div className={`border-b border-[#63676e] p-4 flex ${isOpen ? 'justify-between' : 'justify-center'} items-center`}>
+                        <h1 className={`text-xl font-semibold transition-all overflow-hidden whitespace-nowrap ${isOpen ? '' : 'opacity-0'}`}>Music App</h1>
+                        <button onClick={() => setIsOpen((val: boolean) => !val)} className="cursor-pointer">
+                            <ChevronFirst className={`transition-all duration-300 rounded-lg text-white w-10 h-10 bg-[#63676e] p-1.5 ${isOpen ? '' : 'rotate-180'}`} />
+                        </button>
                     </div>
-                    <Settings className={`cursor-pointer hover:outline rounded-[50%] w-5 h-5 overflow-hidden transition-all ${isOpen ? '' : 'w-0 hidden'}`}/>
+
+                    <SideBarContext.Provider value={{ isOpen }}>
+                        <ul className="flex-1 m-2 overflow-y-auto sidebar-scrollbar">
+                            {children}
+                        </ul>
+                    </SideBarContext.Provider>
+
+
+                    {/* <div className="border-t border-[#63676e] flex justify-center items-center p-2">
+                        
+                        
+                        {!isOpen && (
+                            <Settings className="cursor-pointer hover:rotate-180 transition-all" />
+                        )}
+
+                        
+                        {isOpen && (
+                            <>
+                                <div className="transition-all w-32 ml-2">
+                                    <p>Settings</p>
+                                </div>
+                                <Settings className="cursor-pointer hover:rotate-180 w-6 h-6 transition-all" />
+                            </>
+                        )}
+                    </div> */}
+
                 </div>
             </div>
-        </div>
+
+
+        </>
     )
 }
 
--- React/Project work/project-work/src/pages/MainPage.tsx
+++ React/Project work/project-work/src/pages/MainPage.tsx
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LeftSideBar from "@/components/LeftSideBar"
 import LeftSideBarItem from "@/components/LeftSideBarItem";
-import { Disc3 ,ListMusic, MicVocal, Music, Radio } from "lucide-react";
-
+import { Disc3, House, ListMusic, MicVocal, Music, Music3, Radio, TvMinimal } from "lucide-react";
+import Navbar from "@/components/Navbar";
 
 const MainPage: React.FC = () => {
-    return (
-        <div className="flex bg-[#171719] ">
-            <LeftSideBar>
-                <LeftSideBarItem sectionName="My collection" icon={<Radio/>} text="Mixes and Ratio" active/>
-                <LeftSideBarItem icon={<ListMusic/>} text="Playlists"/>
-                <LeftSideBarItem icon={<Disc3/>} text="Albums"/>
-                <LeftSideBarItem icon={<Music/>} text="Tracks"/>
-                <LeftSideBarItem icon={<MicVocal />} text="Artists"/>
+    // Исправить!!!!!
+    const [isOpen, setIsOpen] = useState(true);
+    const [isMobile, setIsMobile] = useState(false);
 
-            </LeftSideBar>
-        </div>
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    // 
+
+    return (
+        <>
+            <div className="relative flex bg-[#171719]">
+                <LeftSideBar isOpen={isOpen} setIsOpen={setIsOpen}>
+                    <LeftSideBarItem icon={<House />} text="Home" active />
+                    <LeftSideBarItem icon={<Music3 />} text="Explore" />
+                    <LeftSideBarItem icon={<TvMinimal />} text="Videos" />
+                    <LeftSideBarItem sectionName="My collection" icon={<Radio />} text="Mixes and Ratio" />
+                    <LeftSideBarItem icon={<ListMusic />} text="Playlists" />
+                    <LeftSideBarItem icon={<Disc3 />} text="Albums" />
+                    <LeftSideBarItem icon={<Music />} text="Tracks" />
+                    <LeftSideBarItem icon={<MicVocal />} text="Artists" />
+                </LeftSideBar>
+
+                <div className="flex flex-col flex-1">
+                    <Navbar isOpen={isOpen} setIsOpen={setIsOpen}/>
+
+                    <main
+                        className={`
+                        pt-16 p-4 transition-all h-1000
+                        ${isMobile ? 'ml-0' : (isOpen ? 'ml-64' : 'ml-16')}
+                        
+                    `}
+                    >
+тто
+                    </main>
+                </div>
+            </div>
+
+        </>
     )
 }
 
