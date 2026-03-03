afd09f5ef72ec4c478b1b550fce5cc1a3616b3c9
Faik Hasanov <faik.hasanov05@gmail.com>
2026-03-04 03:16:09 +0400

    Delete

--- Next JS/Saudi-arabia-main/app/api/destenations/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { destinations } from "@/data/destenations";
-
-export async function GET() {
-    return NextResponse.json(destinations);  // localhost:3000/api/destenations
-}
--- Next JS/Saudi-arabia-main/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import HomePage from "@/components/pages/main/components/home";
-
-export default function Home() {
-  return (
-    <HomePage />
-  );
-}
